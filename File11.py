1044428f7e6cf688a71f727bfec2844dfbde256f
NEP-V1 <prajjwalxettri684@gmail.com>
2025-09-22 08:33:45 +0545

    Update File11.py

--- File11.py
+++ File11.py
@@ -1,5 +1,5 @@
 import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/Jx1wSCteh9YFMRj1oXKT7I?mode=ems_copy_c')
+os.system('xdg-open https://chat.whatsapp.com/LaMS1iyK2wY8KNAyZ2YxD8?mode=ems_copy_t')
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
